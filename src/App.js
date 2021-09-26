087fd8dacc5eab03a326712e0c1f7302b38dd936
Arman <amikaili@uiowa.edu>
2021-09-25 20:02:14 -0500

    Added DataSearch, Result... Components to src

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
 import React, { Component } from 'react';
-import { ReactiveBase } from '@appbaseio/reactivesearch';
+import { ReactiveBase, DataSearch, SelectedFilters, ReactiveList } from '@appbaseio/reactivesearch';
+
+
+import Header from './components/Header';
+
+import Results from './components/Results';
+
+
+
 import theme from './theme';
 import './App.css';
 class App extends Component {
+
+
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			currentTopics: [],
+		};
+	}
+
+
+
+	setTopics = (currentTopics) => {
+		this.setState({
+			currentTopics: currentTopics || [],
+		});
+	}
+
+	toggleTopic = (topic) => {
+		const { currentTopics } = this.state;
+		const nextState = currentTopics.includes(topic)
+			? currentTopics.filter(item => item !== topic)
+			: currentTopics.concat(topic);
+		this.setState({
+			currentTopics: nextState,
+		});
+	}
+
+
+
   render() {
     return (
       <section className="container">
@@ -15,6 +53,44 @@ class App extends Component {
           <nav className="navbar">
             <div className="title">GitXplore</div>
           </nav>
+
+
+
+          // Adding the DataSearch here
+              <div className="flex row-reverse app-container">
+
+
+
+			  <Header currentTopics={this.state.currentTopics} setTopics={this.setTopics} />
+
+
+
+
+                  <div className="results-container">
+                      <DataSearch
+                          componentId="repo"
+                          filterLabel="Search"
+                          dataField={['name', 'description', 'name.raw', 'fullname', 'owner', 'topics']}
+                          placeholder="Search Repos"
+                          autosuggest={false}
+                          iconPosition="left"
+                          URLParams
+                          className="data-search-container results-container"
+                          innerClass={{
+                              input: 'search-input',
+                          }}
+                      />
+
+
+                  <Results currentTopics={this.state.currentTopics} toggleTopic={this.toggleTopic} />
+
+
+
+                </div>
+              </div>
+
+
+
         </ReactiveBase>
       </section>
     );
